551315ce12ea0acec7892cca043a7988dba322a7
Rian Marcos <rian.sepulveda@hotmail.com>
2023-07-09 15:46:40 -0300

    hoisting exemplo

---
+++ hoisting.js
@@ -0,0 +1,7 @@
+//var a  ---> ele basicamente faz isso
+console.log('a = ',a)
+var a = 2
+console.log('a = ',a)
+
+//Em JS mesmo o programa sendo executado de cima para baixo, nao gera erro ao usar a var antes de a declarar, o LET gera erro
+//não tem pq usar isso, é só uma curiosidade

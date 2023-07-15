b70dbdd7ac0bf13a70a6dfb643a09a8b30997150
Rian Marcos <rian.sepulveda@hotmail.com>
2023-07-15 14:20:17 -0300

    for, percorrendo array

---
+++ exercicios-js/controle/for1.js
@@ -0,0 +1,13 @@
+let contador = 1
+while(contador <=10){
+    contador++
+    console.log(contador)
+}
+
+for(let i = 1; i <= 10; i++){
+    console.log(i)
+}
+const notas =[6.7,7.4,9.8,8.1,7.7]
+for(let i = 0; i < notas.length; i++){ //assim vc percorre todo o array
+    console.log('nota: ' + notas[i])
+}
